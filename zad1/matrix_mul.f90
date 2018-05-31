19f3324c5df26812813509cd920acfdab5f16a44
Wojciech Basiura <basiura.wojciech@gmail.com>
2018-05-31 16:58:56 +0200

    Pierwsze podejscie do mnozenia (zad1).

--- zad1/matrix_mul.f90
+++ zad1/matrix_mul.f90
@@ -13,6 +13,23 @@ contains
         real (kind = 8), intent(out) :: multiply(:,:) ! macierz wynikowa
         integer (kind = 4), intent(out) :: status ! kod błędu, 0 gdy OK
 
+        integer (kind = 4) :: i,j ! zmienne incrementowane w petlach
+
+        ! jezeli wymiary sie nie zgadzaja mnozenie nie ma sensu
+        if(size(first, 2) .NE. size(second, 1)) then
+            status = 1
+            return
+        end if
+
+        multiply = 0
+
+        multiply_columns: do i=1, size(first, 1)
+            multiply_rows: do j=1, size(second, 2)
+                elems: do k=1, size(first, 2)
+                    multiply(i,j) = multiply(i,j) + first(i,k) * second(k,j)
+                end do elems
+            end do multiply_rows
+        end do multiply_columns
         
     end subroutine
 
