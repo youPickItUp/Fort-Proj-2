171ecc5cbbb472c647c7bbb3972ea9b51aef7a93
Wojciech Basiura <basiura.wojciech@gmail.com>
2018-05-31 16:12:34 +0200

    Initial commit. Creating repository.

---
+++ zad1/matrix_mul.f90
@@ -0,0 +1,19 @@
+module matrix_mul
+implicit none
+
+public mm
+private
+
+contains
+
+    subroutine mm(first, second, multiply, status)
+        implicit none
+        real (kind = 8), intent(in) :: first(:,:) ! pierwsza macierz
+        real (kind = 8), intent(in) :: second(:,:) ! druga macierz
+        real (kind = 8), intent(out) :: multiply(:,:) ! macierz wynikowa
+        integer (kind = 4), intent(out) :: status ! kod błędu, 0 gdy OK
+
+        
+    end subroutine
+
+end module matrix_mul
